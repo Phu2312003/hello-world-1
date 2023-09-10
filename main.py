461eb75153cb3ebdb5948d6d61a93f187e7ba074
Hòa Đinh <hoadm.net@gmail.com>
2023-09-10 20:59:59 +0700

    resolve confict in get_age

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from utilities import get_age
 
 if __name__ == '__main__':
 	print('Hello, world!')
+	print('Say hi!')
 	birth_of_year = int(input('BoY: '))
 	print(f"Your age: {get_age(birth_of_year)}")
