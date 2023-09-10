ade2d763d2215833cb9f94e1cb9e1130d104abbf
Hòa Đinh <hoadm.net@gmail.com>
2023-09-10 20:47:09 +0700

    done get age

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from utilities import get_age
+
 
 if __name__ == '__main__':
 	print('Hello, world!')
-
+	birth_of_year = int(input('BoY: '))
+	print(f"Your age: {get_age(birth_of_year)}")
--- utilities.py
+++ utilities.py
@@ -1,4 +1,12 @@
+import datetime
+
+
 def get_full_name():
 	first_name = input("First name: ")
 	last_name = input("Last name: ")
 	return f"{last_name} {first_name}"
+
+
+def get_age(BoY):
+	today = datetime.date.today()
+	return today.year - BoY
